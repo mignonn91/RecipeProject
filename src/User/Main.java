44385e67254a57cf675bbc6de4539d96ea036fa2
Soft43 <Soft43@192.168.200.188>
2021-12-18 00:22:30 +0900

    userloginScreen

---
+++ src/User/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package User;
+
+/**
+ * @author Soft43
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		loginRequest();
+	}
+
+	/**
+	 * 로그인 요청
+	 */
+	private static void loginRequest() {
+		// TODO Auto-generated method stub
+		userloginScreen userLS;
+		userLS = new userloginScreen();
+	}
+
+}
---
+++ src/User/userloginScreen.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+package User;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ * @author Soft43
+ *	
+ */
+public class userloginScreen extends JFrame{
+	
+	//변수 선언하기
+	JLabel label_id = new JLabel("아이디");
+	JLabel label_password = new JLabel("패스워드");
+	JTextField id = new JTextField(15);
+	JTextField password = new JTextField(15);
+	JButton loginbutton = new JButton("로그인");
+	JButton mjbutton = new JButton("회원가입");
+	
+	/**
+	 * ID, PW 입력값 검사
+	 * @param text1
+	 * @param text2
+	 * @return 에러 값
+	 */
+	int IDPWCheck(String text1, String text2) {
+		
+		if (text1.equals("")||text2.equals("")) {
+			JOptionPane.showMessageDialog(null, "모두 입력해 주세요.","에러",JOptionPane.ERROR_MESSAGE);
+			return 1;
+		}
+		return 0;
+	}
+	
+	/**
+	 * 로그인 화면
+	 */
+	public userloginScreen() {
+		// TODO Auto-generated constructor stub
+		
+		//화면 생성
+		setTitle("userloginScreen");
+		setSize(375, 250);
+	    setLocationRelativeTo(null);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setVisible(true);
+		setLayout(null);
+		
+		label_id.setBounds(80,60,50,25);
+		id.setBounds(150,60,150,25);
+		label_password.setBounds(80,95,50,25);
+		password.setBounds(150,95,150,25);
+		
+		loginbutton.setBounds(80,140,85,30);
+		mjbutton.setBounds(200,140,85,30);
+		
+		add(label_id);
+		add(id);
+		add(label_password);
+		add(password);
+		add(loginbutton);
+		add(mjbutton);
+		
+		loginbutton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				if(IDPWCheck(id.getText(), password.getText())==0) {
+//					userLoginPro userLP;
+//					userLP = new userLoginPro;
+				}
+			}
+		});
+		
+		mjbutton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+//				userMemberJoinScreen userMJS;
+//				userMJS = new userMemberJoinScreen;
+			}
+		});
+		
+	}
+
+}
+
